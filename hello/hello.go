9137a6c5a3c3eed4bba8d4e8acf8cfec839bd9f1
hienpdbk <hienpdbk@gmail.com>
2020-06-20 22:44:30 +0900

    update v2.1.1

--- hello/hello.go
+++ hello/hello.go
@@ -5,7 +5,7 @@ import (
 )
 
 func HelloToName(str string, i int) string {
-	return fmt.Sprintf("V1: Hello to %s, %d", str, i)
+	return fmt.Sprintf("V2_update: Hello to %s, %d", str, i)
 }
 
 func test() string {
